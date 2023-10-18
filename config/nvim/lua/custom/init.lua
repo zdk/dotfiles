11c412c0eaec67ebe80f1188f01c760c0814fb42
zdk <zdkaster@gmail.com>
2023-10-18 11:40:02 +0700

    Add comment/doc of oil.nvim keymap for later ref

--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -12,4 +12,20 @@
 
 vim.schedule(function()
   vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  -- default oil.nvim keymap
+  --   ["g?"] = "actions.show_help",
+  --   ["<CR>"] = "actions.select",
+  --   ["<C-s>"] = "actions.select_vsplit",
+  --   ["<C-h>"] = "actions.select_split",
+  --   ["<C-t>"] = "actions.select_tab",
+  --   ["<C-p>"] = "actions.preview",
+  --   ["<C-c>"] = "actions.close",
+  --   ["<C-l>"] = "actions.refresh",
+  --   ["-"] = "actions.parent",
+  --   ["_"] = "actions.open_cwd",
+  --   ["`"] = "actions.cd",
+  --   ["~"] = "actions.tcd",
+  --   ["gs"] = "actions.change_sort",
+  --   ["gx"] = "actions.open_external",
+  --   ["g."] = "actions.toggle_hidden",
 end)
