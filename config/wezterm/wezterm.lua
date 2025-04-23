ec8e659a18c7657c5bd41335b95b3651ca60dd5c
zdk <di.warachet@gmail.com>
2025-04-23 11:58:51 +0700

    wezterm: update config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -13,23 +13,23 @@ config.font = wezterm.font_with_fallback({
 	"Fira Code",
 })
 
-config.font_size = 12
+config.font_size = 13
 config.dpi = 144
 config.max_fps = 240
 config.enable_wayland = false
 config.pane_focus_follows_mouse = false
 config.warn_about_missing_glyphs = false
-
+--
 config.line_height = 1.0
 config.send_composed_key_when_left_alt_is_pressed = false
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.audible_bell = "Disabled"
 config.window_padding = {
-	left = 2,
-	right = 2,
-	top = 2,
-	bottom = 2,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
 }
 config.initial_cols = 200
 config.initial_rows = 100
@@ -38,13 +38,13 @@ config.inactive_pane_hsb = {
 	brightness = 0.8,
 }
 config.enable_scroll_bar = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.window_background_opacity = 1.0
 config.tab_max_width = 50
 config.hide_tab_bar_if_only_one_tab = true
-
+--
 config.adjust_window_size_when_changing_font_size = false
 config.front_end = "OpenGL"
 
