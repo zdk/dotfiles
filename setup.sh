148cc841331c02268f8e95213ee1660e0da1cf00
zdk <di.warachet@gmail.com>
2025-06-08 18:51:06 +0700

    update setup scripts

--- setup.sh
+++ setup.sh
@@ -6,12 +6,17 @@ function link_dot () {
   ln -svf "$CURRENT_PATH/${1}" "${ZDOTDIR:-$HOME}/.${1}"
 }
 
+function unlink_dot () {
+  rm -f "${ZDOTDIR:-$HOME}/.${1}"
+}
+
 # Add any dot file
 files=('zshrc' 'zpreztorc' 'config')
 
 echo "Linking..."
 setopt EXTENDED_GLOB
 for f in $files; do
+  unlink_dot "$f"
   link_dot "$f"
 done
 echo "Done!"
