b2ba202fc9c2400885400c1a3e64caf65536e1fe
zdk <di.warachet@gmail.com>
2025-06-13 13:29:40 +0700

    use zinit
    current zprezto state is not well-maintained.

--- config/nvim/lua/plugins/user/init.lua
+++ config/nvim/lua/plugins/user/init.lua
@@ -106,92 +106,92 @@ return {
     },
   },
 
-  -- {
-  --   "someone-stole-my-name/yaml-companion.nvim",
-  --   requires = {
-  --     { "neovim/nvim-lspconfig" },
-  --     { "nvim-lua/plenary.nvim" },
-  --     { "nvim-telescope/telescope.nvim" },
-  --   },
-  --   config = function()
-  --     local cfg = require("yaml-companion").setup {
-  --       -- detect k8s schemas based on file content
-  --       builtin_matchers = {
-  --         kubernetes = { enabled = true },
-  --       },
-  --
-  --       -- schemas available in Telescope picker
-  --       schemas = {
-  --         -- not loaded automatically, manually select with
-  --         -- :Telescope yaml_schema
-  --         {
-  --           name = "Argo CD Application",
-  --           uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json",
-  --         },
-  --         {
-  --           name = "SealedSecret",
-  --           uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/bitnami.com/sealedsecret_v1alpha1.json",
-  --         },
-  --         -- schemas below are automatically loaded, but added
-  --         -- them here so that they show up in the statusline
-  --         {
-  --           name = "Kustomization",
-  --           uri = "https://json.schemastore.org/kustomization.json",
-  --         },
-  --         {
-  --           name = "GitHub Workflow",
-  --           uri = "https://json.schemastore.org/github-workflow.json",
-  --         },
-  --       },
-  --
-  --       lspconfig = {
-  --         settings = {
-  --           yaml = {
-  --             validate = true,
-  --             format = {
-  --               enable = false,
-  --               singleQuote = true,
-  --               printWidth = 120,
-  --             },
-  --             schemaStore = {
-  --               enable = false,
-  --               url = "",
-  --             },
-  --             -- schemas from store, matched by filename
-  --             -- loaded automatically
-  --             schemas = require("schemastore").yaml.schemas {
-  --               select = {
-  --                 "kustomization.yaml",
-  --                 "GitHub Workflow",
-  --               },
-  --             },
-  --           },
-  --         },
-  --       },
-  --     }
-  --
-  --     require("lspconfig")["yamlls"].setup(cfg)
-  --     require("telescope").load_extension "yaml_schema"
-  --   end,
-  -- },
-  -- {
-  --   "cenk1cenk2/schema-companion.nvim",
-  --   dependencies = {
-  --     { "nvim-lua/plenary.nvim" },
-  --     { "nvim-telescope/telescope.nvim" },
-  --   },
-  --   config = function()
-  --     require("schema-companion").setup {
-  --       -- if you have telescope you can register the extension
-  --       enable_telescope = true,
-  --       matchers = {
-  --         -- add your matchers
-  --         require("schema-companion.matchers.kubernetes").setup { version = "master" },
-  --       },
-  --     }
-  --   end,
-  -- },
-  --
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    requires = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      local cfg = require("yaml-companion").setup {
+        -- detect k8s schemas based on file content
+        builtin_matchers = {
+          kubernetes = { enabled = true },
+        },
+
+        -- schemas available in Telescope picker
+        schemas = {
+          -- not loaded automatically, manually select with
+          -- :Telescope yaml_schema
+          {
+            name = "Argo CD Application",
+            uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json",
+          },
+          {
+            name = "SealedSecret",
+            uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/bitnami.com/sealedsecret_v1alpha1.json",
+          },
+          -- schemas below are automatically loaded, but added
+          -- them here so that they show up in the statusline
+          {
+            name = "Kustomization",
+            uri = "https://json.schemastore.org/kustomization.json",
+          },
+          {
+            name = "GitHub Workflow",
+            uri = "https://json.schemastore.org/github-workflow.json",
+          },
+        },
+
+        lspconfig = {
+          settings = {
+            yaml = {
+              validate = true,
+              format = {
+                enable = false,
+                singleQuote = true,
+                printWidth = 120,
+              },
+              schemaStore = {
+                enable = false,
+                url = "",
+              },
+              -- schemas from store, matched by filename
+              -- loaded automatically
+              schemas = require("schemastore").yaml.schemas {
+                select = {
+                  "kustomization.yaml",
+                  "GitHub Workflow",
+                },
+              },
+            },
+          },
+        },
+      }
+
+      require("lspconfig")["yamlls"].setup(cfg)
+      require("telescope").load_extension "yaml_schema"
+    end,
+  },
+
+  {
+    "cenk1cenk2/schema-companion.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("schema-companion").setup {
+        -- if you have telescope you can register the extension
+        enable_telescope = true,
+        matchers = {
+          -- add your matchers
+          require("schema-companion.matchers.kubernetes").setup { version = "master" },
+        },
+      }
+    end,
+  },
   -- {
   --   "stevearc/conform.nvim",
   --   optional = true,
