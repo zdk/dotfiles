e31909cbb223da29afc3d17450c25d197131d593
zdk <di.warachet@gmail.com>
2025-08-22 21:44:01 +0700

    wezterm: forward-word and backward-word

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,6 +2,13 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
+
 config.font_dirs = { "~/Library/Fonts/" }
 
 config.color_scheme = "Everforest Light (Gogh)"
